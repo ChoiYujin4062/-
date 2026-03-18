3701c67c816f198c3a914a271d857e624887c380
ChoiYujin4062 <cyujin4062@gmail.com>
2026-03-18 15:35:22 +0900

    [Bronze V] Title: 아스키 코드, Time: 112 ms, Memory: 14280 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main{
+	public static void main(String[] args) throws IOException{
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		char ch = reader.readLine().charAt(0);
+		
+		writer.write((int)ch +"");
+		
+		writer.flush();
+	}
+}
